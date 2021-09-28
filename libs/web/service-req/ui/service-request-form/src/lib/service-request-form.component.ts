cd23614eee906ff13b9aacb62c94b4c025015965
karthick_palaniswamy <karthick.p@gmail.com>
2021-09-27 21:07:02 -0400

    service request upload feature added

--- libs/web/service-req/ui/service-request-form/src/lib/service-request-form.component.ts
+++ libs/web/service-req/ui/service-request-form/src/lib/service-request-form.component.ts
@@ -1,7 +1,10 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import {ServiceRequest} from '@whoa/web/service-req/data-access'
+import { ServiceRequest } from '@whoa/web/service-req/data-access';
+import { NzUploadChangeParam, NzUploadFile } from 'ng-zorro-antd/upload';
+import { NzModalService } from 'ng-zorro-antd/modal';
+import { NzMessageService } from 'ng-zorro-antd/message';
 
 @Component({
   selector: 'whoa-service-request-form',
@@ -15,13 +18,15 @@ export class ServiceRequestFormComponent {
   loading = false;
   stateList = [
     { label: 'KS', value: 'KS' },
-    { label: 'NC', value: 'NC'},
-    { label: 'TX', value: 'TX'},
-    { label: 'AL', value: 'AL'},
-    { label: 'AK', value: 'AK'}
+    { label: 'NC', value: 'NC' },
+    { label: 'TX', value: 'TX' },
+    { label: 'AL', value: 'AL' },
+    { label: 'AK', value: 'AK' }
   ];
+  UPLOAD_FILE = 'file://C:/Downloads'; // File upload path - must be rest call
 
-  constructor(fb: FormBuilder, private router: Router) {
+  constructor(fb: FormBuilder, private router: Router, private msg: NzMessageService, private message: NzModalService) {
+
     this.form = fb.group({
       propStreetAddress: [null, [Validators.required]], //, Validators.pattern(/^(user)$/)
       propCity: [null, [Validators.required]], //, Validators.pattern(/^(password)$/)
@@ -29,9 +34,50 @@ export class ServiceRequestFormComponent {
       propZip: [null, [Validators.required]],
       requestedService: [null, [Validators.required]],
       comments: [null, [Validators.maxLength(100)]],
+      attachements: [null, null],
       remember: [true]
     });
   }
+
+  fileList: NzUploadFile[] = [];
+  beforeUpload = (file: NzUploadFile): boolean => {
+    // Judgment on the upload file type
+    const type = file.type;
+
+    const str = ['application/pdf', 'image/jpg', 'image/jpeg', 'image/png'];
+    if (type != null && str.indexOf(type) < 0) {
+      this.message.warning({
+        nzTitle: 'Warning',
+        nzContent: 'Select file failed, only pdf, jpg, jpeg, png and other formats are supported'
+      });
+      return false;
+    }
+
+    if (file.size != null) {
+      // Limit on upload file size
+      const isLt20M = file.size / 1024 / 1024 < 30;
+      if (!isLt20M) {
+        this.message.warning({
+          nzTitle: 'Warning',
+          nzContent: 'The file must be less than 30M'
+        });
+        return false;
+      }
+    }
+    this.fileList = this.fileList.concat(file);
+    // When the type and size meet the requirements, upload directly; if return false, then you need to call the upload method manually
+    return true;
+  }
+  // Method to get the path when the file upload is finished     	 
+  getFileUrl( info: NzUploadChangeParam ): void {
+    const status = info.file.status;
+    if (status === 'done') {
+      //this.zizhi_prove = info.file.response.data
+    } else if (status === 'error') {
+      this.msg.error(`${info.file.name} file upload failed.`);
+    }
+  }
+
 
   serviceReq(): void {
     const propStreetAddress = this.form.controls.propStreetAddress;
@@ -48,7 +94,7 @@ export class ServiceRequestFormComponent {
     if (this.form.invalid) {
       return;
     }
-    
+
     this.submitForm.emit(this.form.value as ServiceRequest);
   }
 }
