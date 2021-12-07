198c891a809d05e04d0bcf3feabead712b9b6fd9
akommineni <>
2021-12-06 20:22:27 -0600

    service request change to use portlet component

--- libs/web/service-req/ui/service-request-form/src/lib/service-request-form.module.ts
+++ libs/web/service-req/ui/service-request-form/src/lib/service-request-form.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { ServiceRequestFormComponent } from './service-request-form.component';
 import { ReactiveFormsModule } from "@angular/forms";
 import { ZorroProviderModule } from "@whoa/web/shared/ui/zorro-provider";
+import { LayoutModule } from "@whoa/web/core/ui/layout";
 
 @NgModule({
-  imports: [CommonModule, ReactiveFormsModule, ZorroProviderModule],
+  imports: [CommonModule, ReactiveFormsModule, ZorroProviderModule, LayoutModule],
   declarations: [
     ServiceRequestFormComponent
   ],
